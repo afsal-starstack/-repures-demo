5fc9e0aef1e5d63faa56b2ed829b973b0e979047
Ananthu Prasad <ananthu.prasad@starstacktech.com>
2026-03-16 05:31:34 -0700

    project creation

---
+++ repures_frontend/src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="bg-red-500 h-screen">
+      <h1 className="text-white text-3xl p-5">
+        Hello Ananthu
+      </h1>
+    </div>
+  );
+}
