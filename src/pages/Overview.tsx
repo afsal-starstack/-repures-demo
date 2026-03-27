e1d15123efd7826de25bfa48bb8ba5dafdb167cc
Ananthu Prasad <ananthu.prasad@starstacktech.com>
2026-03-26 21:58:54 -0700

    overview section

--- src/pages/Overview.tsx
+++ src/pages/Overview.tsx
@@ -1,34 +1,34 @@
-// import PlatformCards from "@/components/overview/PlatformCards";
-// import TodayFocus from "@/components/overview/TodaysSection";
-// import StatsCards from "@/components/overview/StatsCards";
-// import ReputationPulse from "@/components/overview/ReputationPulse";
-// import EarlySignals from "@/components/overview/EarlySignals";
-// import QuickActions from "@/components/overview/QuickActions";
-// import AIInsight from "@/components/overview/AIInsight";
-// import MarketPosition from "@/components/overview/MarketPosition";
+import PlatformCards from "@/components/overview/PlatformCards";
+import TodayFocus from "@/components/overview/TodaysSection";
+import StatsCards from "@/components/overview/StatsCards";
+import ReputationPulse from "@/components/overview/ReputationPulse";
+import EarlySignals from "@/components/overview/EarlySignals";
+import QuickActions from "@/components/overview/QuickActions";
+import AIInsight from "@/components/overview/AIInsight";
+import MarketPosition from "@/components/overview/MarketPosition";
 
-// export default function Overview() {
-//   return (
-//     <div className="space-y-6 mt-[115px]">
-//     <p className="text-[#9CA3AF] font-roboto text-[14px] font-normal leading-[20px]">
-//     Good morning, Ahmed
-//     </p>
-//       <PlatformCards />
+export default function Overview() {
+  return (
+    <div className="space-y-6 mt-[115px]">
+    <p className="text-[#9CA3AF] font-roboto text-[14px] font-normal leading-[20px]">
+    Good morning, Ahmed
+    </p>
+      <PlatformCards />
 
-//       <TodayFocus />
+      <TodayFocus />
 
-//       <StatsCards />
+      <StatsCards />
 
-//       <ReputationPulse />
+      <ReputationPulse />
 
-//       <EarlySignals />
+      <EarlySignals />
 
-//       <QuickActions />
+      <QuickActions />
 
-//       <AIInsight />
+      <AIInsight />
 
-//       <MarketPosition />
+      <MarketPosition />
 
-//     </div>
-//   );
-// }
+    </div>
+  );
+}
